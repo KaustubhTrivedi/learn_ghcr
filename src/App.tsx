0780b5387dd927681a761dad22de2110962e3231
Kaustubh Trivedi <kaus12tri@gmail.com>
2024-12-18 18:40:09 +0000

    Version bump 0.0.7 -> 0.0.8

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p>Woohooooo Things are working!!!!!!</p>
+        <p>New changes here</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
